e4aa4ba6887275ce5a03cf5a05816507ff56c37d
Anna Salcedo <salcedoanna8@gmail.com>
2019-11-25 14:14:15 -0500

    Initial commit

---
+++ src/main/java/org/launchcode/WordsYouKnow/WordsYouKnowApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.WordsYouKnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordsYouKnowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordsYouKnowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/launchcode/WordsYouKnow/WordsYouKnowApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.WordsYouKnow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordsYouKnowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
