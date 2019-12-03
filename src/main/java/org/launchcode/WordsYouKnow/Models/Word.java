f22dbf04fea4616740c33dfb8adee617227c7343
Anna Salcedo <salcedoanna8@gmail.com>
2019-12-03 15:56:42 -0500

    Commented some things out

--- src/main/java/org/launchcode/WordsYouKnow/Models/Word.java
+++ src/main/java/org/launchcode/WordsYouKnow/Models/Word.java
@@ -1,5 +1,7 @@
 package org.launchcode.WordsYouKnow.Models;
 
+import org.aspectj.weaver.loadtime.definition.Definition;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -10,3 +12,31 @@ public class Word {
     @GeneratedValue
     private int id;
 }
+//    private static int nextId = 1;
+//
+//    private Word word;
+//    private Definition definition;
+//
+//    public Word(Word aWord, Definition aDefinition) {
+//        this();
+//
+//        word = aWord;
+//        definition = aDefinition;
+//    }
+//
+//    public Word getWord() {
+//        return word;
+//    }
+//
+//    public void setWord(Word word) {
+//        this.word = word;
+//    }
+//
+//    public Definition getDefinition() {
+//        return definition;
+//    }
+//
+//    public void setDefinition(Definition definition) {
+//        this.definition = definition;
+//    }
+//}
