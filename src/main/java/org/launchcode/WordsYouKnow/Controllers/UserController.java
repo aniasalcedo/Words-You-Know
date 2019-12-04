fdca3087d2d8df543d4f76d9dcba4f133b79774d
Anna Salcedo <salcedoanna8@gmail.com>
2019-12-04 18:28:27 -0500

    Edited User Controller

--- src/main/java/org/launchcode/WordsYouKnow/Controllers/UserController.java
+++ src/main/java/org/launchcode/WordsYouKnow/Controllers/UserController.java
@@ -3,27 +3,62 @@ package org.launchcode.WordsYouKnow.Controllers;
 import org.launchcode.WordsYouKnow.Models.Data.UserDao;
 import org.launchcode.WordsYouKnow.Models.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("user")
 public class UserController {
     // ------------------------
     // PUBLIC METHODS
     // ------------------------
 
-    @RequestMapping("/login")
-    @ResponseBody
-    public String create(String name, String password) {
-        User user = null;
-        try {
-            user = new User(name, password);
-            UserDao.save(user);
-        }
-        catch (Exception exception) {
-            return "Error creating the user";
-        }
-        return "User succesfully created!";
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String add(Model model) {
+        model.addAttribute(new User());
+        model.addAttribute("title","Register");
+        return "myprofile";
     }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute @Valid User user,
+                      Errors errors, String verify) {
+        model.addAttribute(user);
+        boolean passwordsMatch = true;
+        if (user.getPassword() == null || verify == null
+                || !user.getPassword().equals(verify)) {
+            passwordsMatch = false;
+            user.setPassword("");
+            model.addAttribute("verifyError", "Passwords must match");
+        }
+
+        if (!errors.hasErrors() && passwordsMatch) {
+            return "user/index";
+        }
+
+        return "user/add";
+    }
+
+//    @RequestMapping("/login")
+//    @ResponseBody
+//    public String create(String name, String password) {
+//        User user = null;
+//        try {
+//            user = new User(name, password);
+//            UserDao.save(user);
+//        }
+//        catch (Exception exception) {
+//            return "Error creating the user";
+//        }
+//        return "User succesfully created!";
+//    }
     // ------------------------
     // PRIVATE FIELDS
     // ------------------------
