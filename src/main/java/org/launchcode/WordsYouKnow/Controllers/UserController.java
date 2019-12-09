6f425d5660946562faed909444d95dbb5a33f9ce
Anna Salcedo <salcedoanna8@gmail.com>
2019-12-09 14:23:03 -0500

    Working on the registration form

--- src/main/java/org/launchcode/WordsYouKnow/Controllers/UserController.java
+++ src/main/java/org/launchcode/WordsYouKnow/Controllers/UserController.java
@@ -1,68 +1,53 @@
-//package org.launchcode.WordsYouKnow.Controllers;
-//
-//import org.launchcode.WordsYouKnow.Models.Data.UserDao;
-//import org.launchcode.WordsYouKnow.Models.User;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.validation.Errors;
-//import org.springframework.web.bind.annotation.ModelAttribute;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.ResponseBody;
-//
-//import javax.validation.Valid;
-//
-//@Controller
-//@RequestMapping("user")
-//public class UserController {
-//    // ------------------------
-//    // PUBLIC METHODS
-//    // ------------------------
-//
-//    @RequestMapping(value = "add", method = RequestMethod.GET)
-//    public String add(Model model) {
-//        model.addAttribute(new User());
-//        model.addAttribute("title","Register");
-//        return "myprofile";
-//    }
-//
-//    @RequestMapping(value = "add", method = RequestMethod.POST)
-//    public String add(Model model, @ModelAttribute @Valid User user,
-//                      Errors errors, String verify) {
-//        model.addAttribute(user);
+package org.launchcode.WordsYouKnow.Controllers;
+
+import org.launchcode.WordsYouKnow.Models.Data.UserDao;
+import org.launchcode.WordsYouKnow.Models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping(value="")
+public class UserController {
+
+    @Autowired
+    private UserDao UserDao;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/register")
+    public String register(Model model) {
+        model.addAttribute(new User());
+        model.addAttribute("title", "Register");
+        return "register";
+    }
+
+    //====== ADDED THIS TO TRY AND ACTUALLY REGISTER USERS, MAY NOT WORK=====
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    public String add(Model model, @ModelAttribute @Valid User user,
+                      Errors errors) { //add this if needed , String passwordconfirmation
+        if (errors.hasErrors()) {
+            return "register";
+        }
+
+        return "add";
+
+//        model.addAttribute(newUser);
 //        boolean passwordsMatch = true;
-//        if (user.getPassword() == null || verify == null
-//                || !user.getPassword().equals(verify)) {
+//        if (user.getPassword() == null || verifypassword == null
+//                || !user.getPassword().equals(verifypassword)) {
 //            passwordsMatch = false;
 //            user.setPassword("");
-//            model.addAttribute("verifyError", "Passwords must match");
+//            model.addAttribute("verifypassword", "Passwords must match");
 //        }
 //
-//        if (!errors.hasErrors() && passwordsMatch) {
-//            return "user/index";
-//        }
-//
-//        return "user/add";
-//    }
-//
-//    @RequestMapping("/login")
-//    @ResponseBody
-//    public String create(String name, String password) {
-//        User user = null;
-//        try {
-//            user = new User(name, password);
-//            UserDao.save(user);
-//        }
-//        catch (Exception exception) {
-//            return "Error creating the user";
-//        }
-//        return "User succesfully created!";
-//    }
-////     ------------------------
-////     PRIVATE FIELDS
-////     ------------------------
-//
-//    @Autowired
-//    private UserDao UserDao;
-//}
+          UserDao.save(User);
+          User myvaliduser = new User(user.getUsername(), user.getPassword());
+          UserDao.save(myvaliduser);
+    }
+}
--- src/main/java/org/launchcode/WordsYouKnow/Models/Data/UserDao.java
+++ src/main/java/org/launchcode/WordsYouKnow/Models/Data/UserDao.java
@@ -9,5 +9,5 @@ import javax.transaction.Transactional;
 @Repository
 @Transactional
 public interface UserDao extends CrudRepository<User, Integer> {
-//    public User findByName(String name);
+                 public User findByName(String name);
 }
